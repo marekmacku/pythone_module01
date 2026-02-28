10d51d28aac4093e7205a8ee9e81fa39cd01ab0d
marekmacku <marek.macku@seznam.cz>
2026-02-28 18:07:32 +0100

    actually ex1

---
+++ ex1/ft_garden_data.py
@@ -0,0 +1,20 @@
+class Plant:
+    def __init__(self, name: str, height: int, age: int) -> None:
+        self.name = name
+        self.height = height
+        self.age = age
+
+    """Formatted getter for all of the attributes"""
+    def get_info(self) -> str:
+        return f"{self.name}: {self.height}cm, {self.age} days old"
+
+
+if __name__ == "__main__":
+    rose = Plant("Rose", 25, 30)
+    sunflower = Plant("Sunflower", 80, 45)
+    cactus = Plant("Cactus", 15, 120)
+
+    print("=== Garden Plant Registry ===")
+    print(rose.get_info())
+    print(sunflower.get_info())
+    print(cactus.get_info())
